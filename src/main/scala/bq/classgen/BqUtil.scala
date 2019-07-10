798fa1c743bf9e5318ff7f892cc871b6f5f92790
Sato <5at0000.10.40@gmail.com>
2019-07-10 16:23:34 +0900

    refactor utility class

--- src/main/scala/bq/classgen/BqUtil.scala
+++ src/main/scala/bq/classgen/BqUtil.scala
@@ -2,13 +2,41 @@ package bq.classgen
 
 import java.util.UUID
 
+import com.google.api.client.json.jackson2.JacksonFactory
+import com.google.api.services.bigquery.model.{TableFieldSchema, TableSchema}
 import com.google.cloud.bigquery._
 
 object BqUtil {
+
+  private val parser = new JacksonFactory()
+
+  private def dtoTableSchemaToBqSchema(dtoSchema: TableSchema): Schema = {
+    val fromPbMethod =
+      classOf[Schema].getDeclaredMethods.toIterable
+        .find(method => method.getName == "fromPb")
+        .get
+
+    fromPbMethod.setAccessible(true)
+    fromPbMethod.invoke(null, dtoSchema).asInstanceOf[Schema]
+  }
+
+  def convert(jsonSchemaString: String): Schema = {
+    val fieldsListDto =
+      parser
+        .createJsonParser(jsonSchemaString)
+        .parseArray(classOf[java.util.ArrayList[TableFieldSchema]], classOf[TableFieldSchema])
+        .asInstanceOf[java.util.ArrayList[TableFieldSchema]]
+
+    val schemaDto = new TableSchema()
+    schemaDto.setFields(fieldsListDto)
+
+    dtoTableSchemaToBqSchema(schemaDto)
+  }
+
   def createTableUsingJson(datasetId: String, tableId: String, schemaJson: String) = {
     val bigquery        = BigQueryOptions.getDefaultInstance.getService
     val dataset         = bigquery.getDataset(datasetId)
-    val schema          = BqJsonToBqSchema.convert(schemaJson)
+    val schema          = convert(schemaJson)
     val tableDefinition = StandardTableDefinition.of(schema)
     dataset.create(tableId, tableDefinition)
   }
